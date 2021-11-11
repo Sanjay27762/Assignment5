7b9a5c33cd786f65f390ff36d0d51bc934998419
SagarVaiyata <53462108+SagarVaiyata@users.noreply.github.com>
2021-11-11 14:30:46 +0530

    first commit

---
+++ BlazorApp/Data/Character.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BlazorApp.Data
+{
+    public class Character
+    {
+        public string name { get; set; }
+
+        public int age { get; set; }
+
+        public string profession { get; set; }
+
+        public bool isFictional { get; set; }
+    }
+}
---
+++ BlazorApp/Data/WeatherForecastService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        
+    }
+}
